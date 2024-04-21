61b321b0852c8421f62c8ff97ef9d03bfec28422
Homelander Films 🦸‍♂️ <132379808+homelanderfilms@users.noreply.github.com>
2024-04-21 08:42:45 +0200

    Update iframeAdjust.js

--- americanpsycho/iframeAdjust.js
+++ americanpsycho/iframeAdjust.js
@@ -3,14 +3,15 @@ const iframe = document.getElementById("filmFrame");
 function adjustIframe() {
   const width = window.innerWidth;
   const height = window.innerHeight;
-  const isMobile = width <= 768;
 
-  if (isMobile) {
-    iframe.style.top = "70%";
-    iframe.style.transform = "translateY(-55%)";
-  } else {
+  if (width === 2560 && height === 1440) {
     iframe.style.top = "195%";
     iframe.style.transform = "translateY(-65%)";
+    iframe.style.height = "400vh";
+  } else if (width === 1920 && height === 1080) {
+    iframe.style.top = /* значение для 1080p */;
+    iframe.style.transform = "translateY(/* значение для 1080p */)";
+    iframe.style.height = /* значение для 1080p */;
   }
 }
 
