fbc721fa5e82d71b46dc2ba4d22f920d231eb8cc
Homelander Films 🦸‍♂️ <132379808+homelanderfilms@users.noreply.github.com>
2024-04-21 20:46:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function claimBonus() {
-    window.location.href = "https://bit.ly/HOMELANDER1WIN";
-    document.body.style.background = "rgba(0, 0, 0, 0.5)";
-}
-
-document.querySelector('.main-photo').addEventListener('click', function() {
-    document.body.style.background = "rgba(0, 0, 0, 0.5)";
-});
